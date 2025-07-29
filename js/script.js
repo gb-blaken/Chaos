f2c24a791617bd7797befa719799cedff77e8721
gb-blaken <122338986+gb-blaken@users.noreply.github.com>
2025-07-29 20:43:19 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-const ring = document.querySelector('.orbital-ring');
-const planets = ring.querySelectorAll('.planet');
-planets.forEach((planet, i) => {
-  const angle = i * (Math.PI * 2) / planets.length;
-  const radius = 120 + (Math.random() * 20 - 10);
-  const x = Math.cos(angle) * radius;
-  const y = Math.sin(angle) * radius;
-  planet.style.left = `${150 + x}px`;
-  planet.style.top = `${150 + y}px`;
+document.addEventListener("DOMContentLoaded", () => {
+  const ring = document.querySelector('.orbital-ring');
+  const planets = ring.querySelectorAll('.planet');
+  planets.forEach((planet, i) => {
+    const angle = i * (Math.PI * 2) / planets.length;
+    const radius = 120 + (Math.random() * 20 - 10);
+    const x = Math.cos(angle) * radius;
+    const y = Math.sin(angle) * radius;
+    planet.style.left = `${150 + x}px`;
+    planet.style.top = `${150 + y}px`;
+  });
 });
